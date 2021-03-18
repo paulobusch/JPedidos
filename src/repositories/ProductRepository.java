e50dffb737a3b6ac34404c6158cacdbbeb416e58
otavioogo <79700599+otavioogo@users.noreply.github.com>
2021-03-18 10:53:33 -0300

    Update ProductRepository.java

--- src/repositories/ProductRepository.java
+++ src/repositories/ProductRepository.java
@@ -19,121 +19,117 @@ import utils.JPedidosException;
  *
  * @author Paulo
  */
-public class ProductRepository implements IProductRepository {
-    
-    public int id;
-    public Product product;
-    public int order_id;
-    public int qtd;
-    public String created_at;
-    public String updated_at;
+public class ProductRepository {
+  public int id;
+  public String name;
+  public String description;
+  public double price;
+  public int qtd;
+  public String created_at;
+  public String updated_at;
 
-    private DatabaseAdapter _adapter;
-    
-    public ProductRepository(DatabaseAdapter adapter) {
-        _adapter = adapter;
-    }
 
+  public static ProductRepository find(int id) throws SQLException {
+        ConexaoBD connect = new ConexaoBD();
+        ProductRepository findedProductRepository = newProductRepository();
     
-    public static ProductRepositoryt find(int id) throws SQLException {
-            ConexaoBD connect = new ConexaoBD();
-           ProductRepositoryfindedProductRepository = new ProductRepository();
-    
-             String sql = "SELECT * FROM order_products WHERE id = ? limit 1";
+     String sql = "SELECT * FROM Product_Repository WHERE id = ? limit 1";
       
-            PreparedStatement ps = connect.conn.prepareStatement(sql);
-         ps.setInt(1, id);
+    PreparedStatement ps = connect.conn.prepareStatement(sql);
+    ps.setInt(1, id);
 
-         ResultSet returnData = ps.executeQuery();
+    ResultSet returnData = ps.executeQuery();
     
     
     if (returnData.next()) {
       findedProductRepository.id = returnData.getInt("id");
+      findedProductRepository.name = returnData.getString("name");
+      findedProductRepository.description = returnData.getString("description");
+      findedProductRepository.price = returnData.getDouble("price");
       findedProductRepository.qtd = returnData.getInt("qtd");
-      findedProductRepository.product = Product.find(returnData.getInt("product_id"));
-      findedProductRepository.order_id = returnData.getInt("order_id");
       findedProductRepository.created_at = returnData.getString("created_at");
       findedProductRepository.updated_at = returnData.getString("updated_at");
     }
     return findedProductRepository;
   }
 
-    public static List<ProductRepository> list(int orderId) throws SQLException {
+  public static List<ProductRepository> list() throws SQLException {
     ConexaoBD connect = new ConexaoBD();
     
-    String sql = "SELECT * FROM Product_Repository where order_id = ?";
+    String sql = "SELECT * FROM Product_Repository";
       
     PreparedStatement ps = connect.conn.prepareStatement(sql);
-    ps.setInt(1, orderId);
 
     ResultSet returnData = ps.executeQuery();
     
-    List<ProductRepository> findedProductRepository = new ArrayList<ProductRepository>();
-    while (returnData.next()) {
-      ProductRepository findedProductRepository = new ProductRepository();
+        List<ProductRepository> findedProductRepository = new ArrayList<ProductRepository>();
+             while (returnData.next()) {
+            ProductRepository findedProductRepository = new ProductRepository();
 
       findedProductRepository.id = returnData.getInt("id");
+      findedProductRepository.name = returnData.getString("name");
+      findedProductRepository.description = returnData.getString("description");
+      findedPProductRepository.price = returnData.getDouble("price");
       findedProductRepository.qtd = returnData.getInt("qtd");
-      findedProductRepository.product = Product.find(returnData.getInt("product_id"));
-      findedProductRepository.order_id = returnData.getInt("order_id");
       findedProductRepository.created_at = returnData.getString("created_at");
       findedProductRepository.updated_at = returnData.getString("updated_at");
-
-      findedProductRepository.add(findedProductRepository);
+      findedProductRepository.add(findedProduct);
     }
     return findedProductRepository;
   }
-    
-    
+
   public static void create(ProductRepository ProductRepositoryToCreate) throws SQLException {
-        ConexaoBD connect = new ConexaoBD();
+   ConexaoBD connect = new ConexaoBD();
    
-        CurrentDateInString currentDateInString = new CurrentDateInString();
+   CurrentDateInString currentDateInString = new CurrentDateInString();
    
-            String sql = "INSERT INTO order_products (product_id, order_id, qtd, created_at) values (?,?,?,?)";
+   String sql = "INSERT INTO Product_Repository (name, description, price, qtd, created_at) values (?,?,?,?,?)";
      
-                PreparedStatement ps = connect.conn.prepareStatement(sql);
-                ps.setInt(1, ProductRepositoryToCreate.product.id);
-                ps.setInt(2, ProductRepositoryToCreate.order_id);
-                ps.setInt(3, ProductRepositoryToCreate.qtd);
-                ps.setString(4, currentDateInString.execute());
+   PreparedStatement ps = connect.conn.prepareStatement(sql);
+   ps.setString(1, ProductRepositoryToCreate.name);
+   ps.setString(2, ProductRepositoryToCreate.description);
+   ps.setDouble(3, ProductRepositoryToCreate.price);
+   ps.setInt(4, ProductRepositoryToCreate.qtd);
+   ps.setString(5, currentDateInString.execute());
 
-            ps.executeUpdate();
+   ps.executeUpdate();
 
   }
 
-    
-    public static void update(ProductRepository ProductRepositoryToUpdate) throws SQLException {
-        ConexaoBD connect = new ConexaoBD();
+  public static void update(ProductRepository ProductRepositoryToUpdate) throws SQLException {
+   ConexaoBD connect = new ConexaoBD();
    
-            CurrentDateInString currentDateInString = new CurrentDateInString();
+   CurrentDateInString currentDateInString = new CurrentDateInString();
    
-                    String sql = "UPDATE Product_Repository SET product_id=?, order_id=?, qtd=?, updated_at=? WHERE id=?";
+   String sql = "UPDATE Product_Repository SET name=?, description=?, price=?, qtd=?, updated_at=? WHERE id=?";
      
-                            PreparedStatement ps = connect.conn.prepareStatement(sql);
-                            ps.setInt(1, ProductRepositoryToUpdate.product.id);
-                            ps.setInt(2, ProductRepositoryToUpdate.order_id);
-                            ps.setInt(3, ProductRepositoryToUpdate.qtd);
-                            ps.setString(4, currentDateInString.execute());
-                            ps.setInt(5, ProductRepositoryToUpdate.id);
+   PreparedStatement ps = connect.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+   ps.setString(1, ProductRepositoryToUpdate.name);
+   ps.setString(2, ProductRepositoryToUpdate.description);
+   ps.setDouble(3, ProductRepositoryToUpdate.price);
+   ps.setInt(4, ProductRepositoryToUpdate.qtd);
+   ps.setString(5, currentDateInString.execute());
+   ps.setInt(6, ProductRepositoryToUpdate.id);
 
-                System.out.println("before execute");
-                            ps.executeUpdate();
-                System.out.println("after execute");
+   ps.executeUpdate();
 
+   ResultSet returnData = ps.getGeneratedKeys();
+   
+   if (returnData.next()) {
+     System.out.println(returnData.getInt(1));
+   }
   }
 
+  public void delete(ProductRepository ProductRepositoryToDelete) throws SQLException {
+    ConexaoBD connect = new ConexaoBD();
     
-    public void delete(ProductRepository ProductRepositoryToDelete) throws SQLException {
-            ConexaoBD connect = new ConexaoBD();
-    
-                String sql = "DELETE FROM Product_Repository WHERE id = ?";
+    String sql = "DELETE FROM Product_Repository WHERE id = ?";
      
-                PreparedStatement ps = connect.conn.prepareStatement(sql);
-                ps.setInt(1, ProductRepositoryToDelete.id);
+    PreparedStatement ps = connect.conn.prepareStatement(sql);
+    ps.setInt(1, ProductRepositoryToDelete.id);
   
-        ps.executeUpdate();
-    }
+    ps.executeUpdate();
+  }
     
 
 }
